9f39bd332b8dbb1679daace5a84925780baba10d
SimpleCarrot42 <johnaccorso@gmail.com>
2024-09-15 14:05:02 +0200

    Update 1.4 -Create js

---
+++ script.js
@@ -0,0 +1,31 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  if (sidebar.style.display === 'none') {
+    sidebar.style.display = 'flex';
+    hamburgerMenu.style.display = 'none'; // hide hamburger menu
+  } else {
+    sidebar.style.display = 'none';
+    hamburgerMenu.style.display = 'block'; // show hamburger menu
+  }
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  if (sidebar.style.display === 'flex') {
+    sidebar.style.display = 'none';
+    hamburgerMenu.style.display = 'block'; // show hamburger menu
+  } else {
+    sidebar.style.display = 'flex';
+    hamburgerMenu.style.display = 'none'; // hide hamburger menu
+  }
+}
+
+document.addEventListener('click', (event) => {
+  const sidebar = document.querySelector('.sidebar');
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  if (!sidebar.contains(event.target) && sidebar.style.display === 'block') {
+    hideSidebar();
+  }
+});
