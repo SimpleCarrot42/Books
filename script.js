07616f24f8ad24909a097dfcab4db79da4ef3fa8
SimpleCarrot42 <johnaccorso@gmail.com>
2024-10-13 12:58:20 +0200

    Update script.js -MVP

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showSidebar(){
   const sidebar = document.querySelector('.sidebar');
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const hamburgerMenu = document.querySelector('#no-hover');
   if (sidebar.style.display === 'none') {
     sidebar.style.display = 'flex';
     hamburgerMenu.style.display = 'none'; // hide hamburger menu
@@ -12,7 +12,7 @@ function showSidebar(){
 
 function hideSidebar(){
   const sidebar = document.querySelector('.sidebar');
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const hamburgerMenu = document.querySelector('#no-hover');
   if (sidebar.style.display === 'flex') {
     sidebar.style.display = 'none';
     hamburgerMenu.style.display = 'block'; // show hamburger menu
@@ -22,10 +22,10 @@ function hideSidebar(){
   }
 }
 
-document.addEventListener('click', (event) => {
-  const sidebar = document.querySelector('.sidebar');
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
-  if (!sidebar.contains(event.target) && sidebar.style.display === 'block') {
-    hideSidebar();
-  }
-});
+// document.addEventListener('click', (event) => {
+//   const sidebar = document.querySelector('.sidebar');
+//   const hamburgerMenu = document.querySelector('.hamburger-menu');
+//   if (!sidebar.contains(event.target) && sidebar.style.display === 'block') {
+//     hideSidebar();
+//   }
+// });
